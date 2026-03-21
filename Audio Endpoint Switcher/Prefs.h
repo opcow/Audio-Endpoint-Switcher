8e152d004d42014993791270047d22a09586adfb
Mitch Crane <mitch.crane@gmail.com>
2026-03-21 16:39:44 -0400

    Add all the things.

---
+++ Audio Endpoint Switcher/Prefs.h
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <string>
+#include "windows.h"
+#include <map>
+
+enum prefs_sections {
+	globals,
+	device,
+	none
+};
+using namespace std;
+
+struct DevicePrefs
+{
+	UINT KeyMods;
+	UINT KeyCode;
+	std::wstring Name;
+	std::wstring DeviceID;
+	std::wstring HotkeyString;
+	bool HasHotkey;
+	bool IsExcludedFromCycle;
+	bool IsHidden;
+	bool IsPresent;
+
+	DevicePrefs() :
+		KeyMods(0),
+		KeyCode(0),
+		Name(),
+		DeviceID(),
+		HotkeyString(),
+		HasHotkey(false),
+		IsExcludedFromCycle(false),
+		IsHidden(false),
+		IsPresent(false)
+	{}
+};
+
+class CQSESPrefs
+{
+
+public:
+
+	// class config
+	void Add(DevicePrefs *sdi);
+	void Update(DevicePrefs *sdi);
+	bool SetMax(int max);
+	int GetMax() { return mMax; };
+	int GetCount() { return mNext; };
+	// hotkey
+	void SetHotkeyString(int index, const wstring & keystring) { if (index < mNext) mDevices[index].HotkeyString = keystring; }
+	wstring& GetHotkeyString(int index, wstring & keyString);
+	void SetHotkeyCode(int index, UINT code);
+	UINT GetHotkeyCode(int index) { if (index < mNext) return mDevices[index].KeyCode; else return 0; }
+	void SetHotkeyMods(int index, UINT mods);
+	UINT GetHotkeyMods(int index);
+	void EnableHotkey(int index, bool enabled) { if (index < mNext) mDevices[index].HasHotkey = enabled; }
+	bool GetHotkeyEnabled(int index);
+	// cycle
+	void SetExcludeFromCycle(int index, bool b) { if (index < mNext) mDevices[index].IsExcludedFromCycle = b; }
+	bool GetExcludeFromCycle(int index);
+	void SetCycleKeyString(const WCHAR * s) { mCycleKeyString = s; }
+	wstring& GetCycleKeyString(wstring& s) { return s = mCycleKeyString; }
+	void SetCycleKeyCode(UINT key) { mKeyCode = key; }
+	UINT GetCycleKeyCode() { return mKeyCode; }
+	void SetCycleKeyMods(UINT mods) { mKeyMods = mods; }
+	UINT GetCycleKeyMods() { return mKeyMods; }
+	void EnableCycleKey(bool enabled) { mIsCycleKeyEnabled = enabled; }
+	bool GetCycleKeyEnabled() { return mIsCycleKeyEnabled; }
+	// misc
+	void SetIsHidden(int index, bool b) { if (index < mNext) mDevices[index].IsHidden = b; }
+	bool GetIsHidden(int index);
+	void SetIsPresent(int index, bool b) { if (index < mNext) mDevices[index].IsPresent = b; }
+	bool GetIsPresent(int index);
+	//void SetDefault(int index) { if (index < mNext) mDefault = index; }
+	//int GetDefault() { return mDefault; }
+	void Swap(int d1, int d2);
+	void Sort();
+	// device info
+	bool GetByName(wstring& name, DevicePrefs * sdi);
+	int FindByID(const wstring& id);
+	wstring& GetName(int index, wstring & s);
+	wstring& GetID(int index, wstring& s);
+	// prefs class
+	bool Init(int max);
+	void Clear(int index);
+	void ResetPresent();
+	// load & save
+	bool Load();
+	bool Save();
+
+	CQSESPrefs( const CQSESPrefs& other );
+	CQSESPrefs() : mDevices(0), mNext(0), mMax(10), /*mDefault(0),*/ mKeyCode(0),
+							mKeyMods(0), mCycleKeyString(L""), mIsCycleKeyEnabled(false),
+							mDevicesHaveChanged(false) { }
+	~CQSESPrefs() { delete[] mDevices; }
+	CQSESPrefs& operator=(const CQSESPrefs& source);
+
+private:
+
+	void Erase(int index);
+	//bool Insert(int index, DevicePrefs * sdi);
+	void Remove(int index);
+	void RemoveDupes();
+	int FindByName(wstring & name);
+	bool ReadConfig(const wstring &fileName, map <wstring, map <wstring, wstring> > & sections);
+
+	DevicePrefs * mDevices;
+	int mMax;
+	int mNext;
+	//int mDefault;
+	UINT mKeyCode;
+	UINT mKeyMods;
+	wstring mCycleKeyString;
+	bool mIsCycleKeyEnabled;
+	bool mDevicesHaveChanged; // not used
+	static const int mLimit = 16;
+
+};
---
+++ Audio Endpoint Switcher/resource.h
@@ -0,0 +1,47 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by aes.rc
+//
+#define VS_VERSION_INFO                 1
+#define IDS_APP_TITLE                   101
+#define IDC_AES                         102
+#define IDS_APPTOOLTIP                  103
+#define IDI_SMALL                       103
+#define IDR_AES_MENU                    104
+#define IDS_STRING_DEVICE               104
+#define IDR_MENU1                       105
+#define IDD_DIALOG_ABOUT                106
+#define IDD_DIALOG_SETTINGS             108
+#define IDI_ICON1                       119
+#define IDC_AES_ICON                    1001
+#define IDC_EDIT_KEY                    1003
+#define IDC_COMBO1                      1014
+#define IDC_EDIT_KEY2                   1015
+#define IDC_CHECK_ENABLE_CYCLE          1016
+#define IDC_CHECK_ENABLE_HOTKEY         1017
+#define IDC_BUTTON_DELETE               1018
+#define IDC_STATIC_APP                  1019
+#define IDC_CHECK_SHIFT_H               1020
+#define IDC_CHECK_CONTROL_H             1021
+#define IDC_CHECK_ALT_H                 1022
+#define IDC_CHECK_WIN_H                 1023
+#define IDC_CHECK_SHIFT_C               1024
+#define IDC_CHECK_CONTROL_C             1025
+#define IDC_CHECK_ALT_C                 1026
+#define IDC_CHECK_WIN_C                 1027
+#define IDC_CHECK_CYCLE                 1028
+#define IDC_CHECK_HIDE                  1029
+#define IDC_CHECK1                      1030
+#define IDC_CHECK_AUTOSTART             1030
+#define ID_AES_ABOUT                    40001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        122
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1031
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
